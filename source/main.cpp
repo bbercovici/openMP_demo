c7c92b734f1fb1419399e4b6c442db0f912781b0
Benjamin Bercovici <benjamin.bercovici@gmail.com>
2017-09-02 12:40:27 -0600

    added percentage comparison

--- source/main.cpp
+++ source/main.cpp
@@ -9,10 +9,11 @@ void demo_omp(double * average_run_time, int runs) {
 
 	std::chrono::time_point<std::chrono::system_clock> start, end;
 	std::chrono::duration<double> elapsed_seconds ;
-	unsigned int N = 2000000;
+	unsigned int N = 10000000;
 
 	// Array assignment without OMP
-	int array[N];
+	int * array = new int[N];
+
 	start = std::chrono::system_clock::now();
 	for (unsigned int i = 0; i < N; ++i) {
 		array[i] = i;
@@ -23,7 +24,7 @@ void demo_omp(double * average_run_time, int runs) {
 
 
 	// Array assignment with OMP
-	int array_omp[N];
+	int * array_omp = new int[N];
 	start = std::chrono::system_clock::now();
 	#pragma omp parallel for
 	for (unsigned int i = 0; i < N; ++i) {
@@ -51,11 +52,15 @@ void demo_omp(double * average_run_time, int runs) {
 	for (unsigned int i = 0; i < N; ++i) {
 		sum_omp += array_omp[i];
 	}
-
 	end = std::chrono::system_clock::now();
 	elapsed_seconds = end - start;
 	average_run_time[3] += elapsed_seconds.count() / runs;
 	assert(sum_omp == sum);
+
+
+	delete[](array);
+	delete[](array_omp);
+
 }
 
 
@@ -79,16 +84,21 @@ int main() {
 
 
 	std::cout << "Average assignement times: " << std::endl;
-	std::cout << "\t Without OMP " << average_run_time[0] << " s" <<  std::endl;
-	std::cout << "\t With OMP " << average_run_time[1] << " s" <<  std::endl;
-	std::cout << "\t Difference: " << average_run_time[0] - average_run_time[1] << " s in favor of OMP" <<  std::endl;
+	std::cout << "\t Without OMP: " << average_run_time[0] << " s" <<  std::endl;
+	std::cout << "\t With OMP: " << average_run_time[1] << " s" <<  std::endl;
+	if (average_run_time[1] < average_run_time[0]) {
+		std::cout << "\t Absolute difference: " << average_run_time[0] - average_run_time[1] << " s in favor of OMP" <<  std::endl;
+		std::cout << "\t Relative difference: OMP is " << 100 * abs(average_run_time[1] - average_run_time[0])/average_run_time[0] << " % faster" <<  std::endl;
 
-
-
+	}
 	std::cout << "Average summation times: " << std::endl;
-	std::cout << "\t Without OMP " << average_run_time[2] << " s" <<  std::endl;
-	std::cout << "\t With OMP " << average_run_time[3] << " s" <<  std::endl;
-	std::cout << "\t Difference: " << average_run_time[2] - average_run_time[3] << " s in favor of OMP" <<  std::endl;
+	std::cout << "\t Without OMP: " << average_run_time[2] << " s" <<  std::endl;
+	std::cout << "\t With OMP: " << average_run_time[3] << " s" <<  std::endl;
+	if (average_run_time[3] < average_run_time[2]) {
+		std::cout << "\t Absolute difference: " << average_run_time[2] - average_run_time[3] << " s in favor of OMP" <<  std::endl;
+		std::cout << "\t Relative difference: OMP is " << 100 * abs(average_run_time[3] - average_run_time[2])/average_run_time[2] << " % faster" <<  std::endl;
+		
+	}
 
 
 
